dc0bebc858166ba678ae89d6584fcd4dc6b23473
Andre_Lima <54004597+fre200tey623@users.noreply.github.com>
2022-09-15 18:20:08 -0300

    Testei novamente
    F

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, sdfhaoisfhjalpsikfhaolsifkhnalsuofgh!");
+        System.out.println("Hello, sdfhaoisfhj!!!!!!!!!!!!!");
     }
 }
