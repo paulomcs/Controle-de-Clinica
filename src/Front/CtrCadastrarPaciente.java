253ecbdc0247aeac163608c0b243e246cf48d1a6
paulomcs <52682759+paulomcs@users.noreply.github.com>
2022-09-25 21:40:44 -0300

    Add files via upload

---
+++ src/Front/CtrCadastrarPaciente.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Back;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Márcia
+ */
+public class CtrCadastrarPaciente {
+        private static Connection  c;
+
+    public CtrCadastrarPaciente(){
+        c = Conexao.getConnection();
+    }
+
+    
+    
+    //public static void exibirTabelaCompleta() throws SQLException { //exibe no console a tabela banco.perfil completa
+    //    Statement st = c.createStatement();
+    //    String sql = "select * from banco.perfil";
+    //    
+    //    ResultSet rs = st.executeQuery(sql);
+    //    System.out.println("___Exibindo_Tabela___");
+    //    System.out.println("|  id  |    valor   |");
+    //    while (rs.next()){
+    //        System.out.format("| %4s | %10s |\n", rs.getString(1), rs.getString(2));
+    //    }
+    //    System.out.println("_____________________");
+    //}
+
+    public static boolean inserirPacienteNaTabela(String cpf, String nome, String telefone){  //insere um valor na tabela
+        new CtrCadastrarPaciente();
+        Statement st;
+            try {
+                st = c.createStatement();
+                String sql = "insert into hospital.paciente (cpf, nome, telefone) values (\'" + cpf + "\',\'" + nome + "\',\'" + telefone + "\')";
+                st.execute(sql);
+                return true;
+            } catch (SQLException ex) {
+                return false;
+            }
+        
+        
+    }
+
+    //public static void atualizarValorNaTabela(int id, int valor) throws SQLException {  //atualiza um valor na tabela
+    //    Statement st = c.createStatement();
+    //    String sql = "update banco.perfil set valor = " + valor + "where id = " + id;
+    //    st.execute(sql);
+    //}
+
+
+
+    public static void main(String[] args) throws SQLException {
+        
+    }
+}
